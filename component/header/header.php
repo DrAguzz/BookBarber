a4d1259917704b1f1043d8cb53c7ba72ee85f867
Aguzzz <erfanbagus06@gmail.com>
2024-11-01 02:00:21 +0800

    Bbook 1
    Header And Navigation

---
+++ component/header/header.php
@@ -0,0 +1,9 @@
+
+<div class="header">
+        <div class="img">
+            <img src="<?php echo $link; ?>img/barber-tools-pattern1.jpg" alt="Header Image">
+        </div>
+        <div class="text"><h1 class="k2d-bold">Barber Book</h1></div>
+        <!-- Admin login link -->
+        <a href="admin_login.html" class="admin-link">Admin</a>
+    </div>
--- landingPage.php
+++ landingPage.php
@@ -1,19 +1,25 @@
+<?php
+    $link = "./";
+?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400&family=K2D:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet">
-<body bgcolor="#2F1D00">
-    <div class="container">
-    <h1>Hello World</h1>
-    </div>
-    <div class="content">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="./css/main.css">
+        <link rel="stylesheet" href="./css/component/component.css">
+        <link rel="stylesheet" href="./css/landingPage.css">
+        <link rel="stylesheet" href="./css/font/font.css">
 
-    </div>
-</body>
+        <title>Document</title>
+    </head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400&family=K2D:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet">
+    <body bgcolor="#2F1D00">
+        <?php 
+            require("./component/header/header.php");
+            require("./component/navbar/nav.html");
+        ?>
+    </body>
 </html>
